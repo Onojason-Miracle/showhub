939c25bbd9b02df1d44a81cb04cad62575108341
Onojason Miracle Ojonuyo <onojasonmiracle@gmail.com>
2023-06-26 18:07:10 +0100

    pages update

--- src/components/Page6.jsx
+++ src/components/Page6.jsx
@@ -3,7 +3,7 @@ import Homenav from "./Homenav";
 import Homefooter from "./Homefooter";
 import { Link, useNavigate } from "react-router-dom";
 
-function Page4() {
+function Page6() {
   const [data, setData] = useState([]);
   const navigate = useNavigate();
   const [selectedMovie, setSelectedMovie] = useState(null);
